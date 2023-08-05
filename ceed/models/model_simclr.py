766d662cd99aba78faac99e4ff8094a901db0500
Ankit Vishnubhotla <ankit.vishnu23@gmail.com>
2023-08-05 13:50:07 -0400

    added cebra backbone

--- ceed/models/model_simclr.py
+++ ceed/models/model_simclr.py
@@ -211,6 +211,109 @@ class FullyConnectedEnc(nn.Module):
         self.load_state_dict(new_state_dict)
         return self
 
+
+# (c) All rights reserved. ECOLE POLYTECHNIQUE FÉDÉRALE DE LAUSANNE,
+# Switzerland, Laboratory of Prof. Mackenzie W. Mathis (UPMWMATHIS) and
+# original authors: Steffen Schneider, Jin H Lee, Mackenzie W Mathis. 2023.
+#
+# Source code:
+# https://github.com/AdaptiveMotorControlLab/CEBRA
+class _Norm(nn.Module):
+
+    def forward(self, inp: torch.Tensor) -> torch.Tensor:
+        return inp / torch.norm(inp, dim=1, keepdim=True)
+    
+
+class Squeeze(nn.Module):
+    """Squeeze 3rd dimension of input tensor, pass through otherwise."""
+
+    def forward(self, inp: torch.Tensor) -> torch.Tensor:
+        """Squeeze 3rd dimension of input tensor, pass through otherwise.
+
+        Args:
+            inp: 1-3D input tensor
+
+        Returns:
+            If the third dimension of the input tensor can be squeezed,
+            return the resulting 2D output tensor. If input is 2D or less,
+            return the input.
+        """
+        if inp.dim() > 2:
+            return inp.squeeze(2)
+        return inp
+
+    
+class _Skip(nn.Module):
+    """Add a skip connection to a list of modules
+
+    Args:
+        *modules (torch.nn.Module): Modules to add to the bottleneck
+        crop (tuple of ints): Number of timesteps to crop around the
+            shortcut of the module to match the output with the bottleneck
+            layers. This can be typically inferred from the strides/sizes
+            of any conv layers within the bottleneck.
+    """
+
+    def __init__(self, *modules, crop=(1, 1)):
+        super().__init__()
+        self.module = nn.Sequential(*modules)
+        self.crop = slice(
+            crop[0],
+            -crop[1] if isinstance(crop[1], int) and crop[1] > 0 else None)
+
+    def forward(self, inp: torch.Tensor) -> torch.Tensor:
+        """Compute forward pass through the skip connection.
+
+        Implements the operation ``self.module(inp[..., self.crop]) + skip``.
+
+        Args:
+            inp: 3D input tensor
+
+        Returns:
+            3D output tensor of same dimension as `inp`.
+        """
+        skip = self.module(inp)
+        return inp[..., self.crop] + skip
+
+
+class CEBRA(nn.Module):
+    def __init__(self, num_units=32, out_size = 2, proj_dim=5, fc_depth=2, input_size=121, multichan=False):
+        super(Encoder, self).__init__()
+        print("init CEBRA Encoder")
+        self.proj_dim = out_size if out_size < proj_dim else proj_dim
+        self.multichan = multichan
+        self.input_size = input_size
+        self.enc_block1d = nn.Sequential(
+            nn.Conv1d(in_channels=self.input_size, out_channels=num_units, kernel_size=2),
+            nn.GELU(),
+            _Skip(nn.Conv1d(num_units, num_units, 3), nn.GELU()),
+            _Skip(nn.Conv1d(num_units, num_units, 3), nn.GELU()),
+            _Skip(nn.Conv1d(num_units, num_units, 3), nn.GELU()),
+            nn.Conv1d(in_channels=num_units, out_channels=out_size, kernel_size=3),
+            _Norm(),
+            Squeeze(),
+        )
+
+        self.projector = Projector(rep_dim=out_size, proj_dim=self.proj_dim)
+
+    def forward(self, x):
+        if self.multichan:
+            x = x.view(-1, 1, self.input_size)
+        x = self.enc_block1d(x)
+        x = self.projector(x)
+        return x
+
+    def load(self, fname_model):
+        checkpoint = torch.load(fname_model, map_location="cpu")
+        state_dict = checkpoint["state_dict"]
+        new_state_dict = OrderedDict()
+        for key in state_dict:
+            # if "backbone" in key and "fc" not in key:
+            new_key = '.'.join(key.split('.')[1:])
+            new_state_dict[new_key] = state_dict[key]
+        self.load_state_dict(new_state_dict)
+        return self
+
 class PositionalEncoding(nn.Module):
 
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000, num_chans: int = 1):
@@ -437,219 +540,6 @@ class MultiChanAttentionEnc1(nn.Module):
         self.load_state_dict(new_state_dict)
         return self
     
-def conv3x3(in_planes, out_planes):
-    """3x3 convolution with padding"""
-    return nn.Conv1d(in_planes, out_planes)
-
-
-def conv1x1(in_planes, out_planes):
-    """1x1 convolution"""
-    return nn.Conv1d(in_planes, out_planes, kernel_size=1)
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None):
-        super(BasicBlock, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm1d
-        if groups != 1 or base_width != 64:
-            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
-        if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck(nn.Module):
-    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
-    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
-    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
-    # This variant is also known as ResNet V1.5 and improves accuracy according to
-    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
-
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None):
-        super(Bottleneck, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        width = int(planes * (base_width / 64.)) * groups
-        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv1x1(inplanes, width)
-        self.bn1 = norm_layer(width)
-        self.conv2 = conv3x3(width, width, stride, groups, dilation)
-        self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, planes * self.expansion)
-        self.bn3 = norm_layer(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class ResNet(nn.Module):
-
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
-                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
-                 norm_layer=None):
-        super(ResNet, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
-
-        self.inplanes = 64
-        self.dilation = 1
-        if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
-            replace_stride_with_dilation = [False, False, False]
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError("replace_stride_with_dilation should be None "
-                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
-        self.groups = groups
-        self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
-                                       dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
-                                       dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
-                                       dilate=replace_stride_with_dilation[2])
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
-        norm_layer = self._norm_layer
-        downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
-                            self.base_width, previous_dilation, norm_layer))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, groups=self.groups,
-                                base_width=self.base_width, dilation=self.dilation,
-                                norm_layer=norm_layer))
-
-        return nn.Sequential(*layers)
-
-    def _forward_impl(self, x):
-        # See note [TorchScript super()]
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
-
-        return x
-
-    def forward(self, x):
-        return self._forward_impl(x)
-
-
-def _resnet(block, layers, **kwargs):
-    model = ResNet(block, layers, **kwargs)
-    return model
-
-
-def resnet18(pretrained=False, progress=True, **kwargs):
-    r"""ResNet-18 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], pretrained, progress,
-                   **kwargs)
-    
 
 model_dict = { "custom_encoder": Encoder,
                            "custom_encoder2": Encoder2,
@@ -657,6 +547,7 @@ model_dict = { "custom_encoder": Encoder,
                             "fc_encoder": FullyConnectedEnc,
                             "attention": AttentionEnc,
                             "attention_multichan": MultiChanAttentionEnc1,
+                            "cebra": CEBRA,
                             # "resnet18": models.resnet18(pretrained=False, num_classes=out_dim),
                             # "resnet50": models.resnet50(pretrained=False, num_classes=out_dim)
                             }
