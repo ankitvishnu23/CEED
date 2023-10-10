fix circular imports and remove normalization into unit sphere

ceed/models/model_simclr.py
@@ -8,34 +8,6 @@ from ceed.models.exceptions import InvalidBackboneError
 from collections import OrderedDict
 import sys
 
-class SingleChanDenoiser(nn.Module):
-    """Cleaned up a little. Why is conv3 here and commented out in forward?"""
-
-    def __init__(
-        # self, n_filters=[16, 8, 4], filter_sizes=[5, 11, 21], spike_size=121
-        self, n_filters=[16, 8], filter_sizes=[5, 11], spike_size=121
-    ):
-        super().__init__()
-        self.conv1 = nn.Sequential(nn.Conv1d(1, n_filters[0], filter_sizes[0]), nn.ReLU())
-        self.conv2 = nn.Sequential(nn.Conv1d(n_filters[0], n_filters[1], filter_sizes[1]), nn.ReLU())
-        if len(n_filters) > 2:
-            self.conv3 = nn.Sequential(nn.Conv1d(n_filters[1], n_filters[2], filter_sizes[2]), nn.ReLU())
-        n_input_feat = n_filters[1] * (spike_size - filter_sizes[0] - filter_sizes[1] + 2)
-        self.out = nn.Linear(n_input_feat, spike_size)
-
-    def forward(self, x):
-        x = x[:, None]
-        x = self.conv1(x)
-        x = self.conv2(x)
-        # x = self.conv3(x)
-        x = x.view(x.shape[0], -1)
-        return self.out(x)
-
-    def load(self, fname_model):
-        checkpoint = torch.load(fname_model, map_location="cpu")
-        self.load_state_dict(checkpoint, strict=False)
-        return self
-
 
 class Projector(nn.Module):
     ''' Projector network accepts a variable number of layers indicated by depth.
@@ -156,15 +128,10 @@ class Encoder2(nn.Module):
         list_layers += [Projector(rep_dim=out_size, proj_dim=self.proj_dim)]
         
         self.fcpart = nn.Sequential(*list_layers)
-        
-        # nn.Sequential(
-        #     nn.Linear(Lv[2] * 1 * 1, Lv[3]),
-        #     nn.ReLU(),
-        #     # nn.Dropout(p=0.2),
-        #     nn.Linear(Lv[3], out_size),
-        #     )
+
         self.Lv = Lv
         # self.projector = Projector2(rep_dim=out_size, proj_dim=self.proj_dim)
+        
     def forward(self, x):
         x = self.enc_block1d(x)
         # print(x.shape)
@@ -471,17 +438,6 @@ class MultiChanAttentionEnc1(nn.Module):
             
         self.fcpart = nn.Sequential(*list_layers)
         
-        # self.fcpart = nn.Sequential(
-        #     nn.Linear(self.spike_size * expand_dim, self.spike_size),
-        #     nn.ReLU(),
-        #     nn.Linear(self.spike_size, out_size),
-            
-        #     # nn.ReLU(),
-        #     # nn.Dropout(p=0.2),
-        #     # nn.Linear(5 * self.spike_size * expand_dim, out_size),
-        #     Projector(rep_dim=out_size, proj_dim=self.proj_dim)
-        # )
-
     def init_weights(self) -> None:
         initrange = 0.1
         # self.encoder.weight.data.uniform_(-initrange, initrange)
@@ -496,18 +452,6 @@ class MultiChanAttentionEnc1(nn.Module):
         Returns:
             output Tensor of shape [batch_size, proj_dim]
         """
-        # src = torch.transpose(src, 1, 2)
-
-        # for chan in range(self.n_channels):
-        #     curr_chan = src[:, :, chan]
-        #     curr_chan = torch.unsqueeze(curr_chan, dim=2)
-        #     # curr_chan = torch.transpose(curr_chan, 1, 2)
-        #     if self.expand_dim != 1:
-        #         curr_chan = self.encoder(curr_chan) * math.sqrt(self.expand_dim)
-        #     curr_chan = self.pos_encoder(curr_chan)
-        #     curr_chan = self.transformer_encoder(curr_chan, src_mask)
-        #     # src[:, chan] = torch.transpose(curr_chan, 1, 2)
-        #     src[:, :, chan] = curr_chan
         
         if self.expand_dim != 1:
             src = torch.unsqueeze(src, dim=-1)
@@ -516,8 +460,6 @@ class MultiChanAttentionEnc1(nn.Module):
         src = src.permute(1, 0, 2) # remove batch_first argument needs Batch in second dim
         
         output = self.transformer_encoder(src, src_mask)
-        # output = output.view(-1, self.n_channels, self.spike_size, self.expand_dim)
-        # output = torch.transpose(src, 1, 2)
         output = output.permute(1, 0, 2)
         output = output.reshape(-1, self.n_channels, self.spike_size * self.expand_dim)
         output = torch.transpose(output, 1, 2)
@@ -543,13 +485,10 @@ class MultiChanAttentionEnc1(nn.Module):
 
 model_dict = { "custom_encoder": Encoder,
                            "custom_encoder2": Encoder2,
-                            "denoiser": SingleChanDenoiser,
                             "fc_encoder": FullyConnectedEnc,
                             "attention": AttentionEnc,
                             "attention_multichan": MultiChanAttentionEnc1,
                             "cebra": CEBRA,
-                            # "resnet18": models.resnet18(pretrained=False, num_classes=out_dim),
-                            # "resnet50": models.resnet50(pretrained=False, num_classes=out_dim)
                             }
 
 class ModelSimCLR(nn.Module):
@@ -561,25 +500,18 @@ class ModelSimCLR(nn.Module):
             self.backbone = model_dict[base_model](out_size=out_dim, proj_dim=proj_dim, fc_depth=fc_depth, expand_dim=expand_dim, cls_head=cls_head)
         else:
             self.backbone = model_dict[base_model](out_size=out_dim, proj_dim=proj_dim, fc_depth=fc_depth, input_size=input_size, multichan=multichan)
-        print(self.backbone)
-        # self.backbone = self._get_basemodel(base_model)
         print("number of encoder params: ", sum(p.numel() for p in self.backbone.parameters()))
         print("number of transfomer params: ", sum(p.numel() for n,p in self.backbone.named_parameters() if 'transformer_encoder' in n))
         print("number of fcpart params: ", sum(p.numel() for n,p in self.backbone.named_parameters() if ('fcpart' in n and 'proj' not in n)))
         print("number of Proj params: ", sum(p.numel() for n,p in self.backbone.named_parameters() if ('fcpart' in n and 'proj' in n)))
         print("number of classifier params: ", sum(p.numel() for n,p in self.backbone.named_parameters() if 'cls_head' in n))
-        
-        
-        if base_model == "denoiser":
-            # add mlp projection head
-            self.backbone.fc = nn.Sequential(self.backbone.fc, Projector(rep_dim=out_dim, proj_dim=proj_dim))
 
     def _get_basemodel(self, model_name):
         try:
             model = self.model_dict[model_name]
         except KeyError:
             raise InvalidBackboneError(
-                "Invalid backbone architecture. Check the config file and pass one of: basic_backbone, resnet18, or resnet50")
+                "Invalid backbone architecture")
         else:
             return model
 

utils/utils.py
@@ -161,7 +161,6 @@ def get_torch_reps(net, data_loader, device, args):
                 dim = 1
             else:
                 dim = 0
-            feature = F.normalize(feature, dim=dim)
             feature_bank.append(feature)
             feature_labels = torch.cat((feature_labels, target))
         # [D, N]
@@ -188,7 +187,6 @@ def get_torch_reps_nolabels(net, data_loader, device, args):
                 dim = 1
             else:
                 dim =0
-            feature = F.normalize(feature, dim=dim)
             feature_bank.append(feature)
         # [D, N]
         feature_bank = torch.cat(feature_bank, dim=0).cpu().numpy()
@@ -352,3 +350,4 @@ def save_reps(model, loader, ckpt_path, split='train', multi_chan=False,rep_afte
         else:
             torch.save(feature_bank, os.path.join(ckpt_root_dir, f'{split}_reps{suffix}.pt'))
             print(f"saved {split} features to {ckpt_root_dir}/{split}_reps{suffix}.pt")
+            