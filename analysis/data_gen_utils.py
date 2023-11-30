0be2c2f219c27b1dd44efefc4723b3e890ee72f1
n0aht <n.telerman@gmail.com>
2023-11-30 15:59:24 -0500

    fixed .stream file name error

--- analysis/data_gen_utils.py
+++ analysis/data_gen_utils.py
@@ -19,6 +19,7 @@ from pathlib import Path
 from tqdm.auto import trange
 import shutil
 import spikeinterface.full as si
+import os
 
 try:
     import brainbox.io.one as bbone
@@ -493,9 +494,9 @@ def download_IBL(
     # Use IBL streamer to download the data
     sr = Streamer(pid=pid, one=one, cache_folder=cache_folder, remove_cached=overwrite, typ=band)
     sr._download_raw_partial(first_chunk=t_window[0], last_chunk=t_window[1] - 1)
-    sr.file_bin = sr.target_dir / "_spikeglx_ephysData_g0_t0.imec0.ap.stream.cbin"
+    sr.file_bin = sr.target_dir / "_spikeglx_ephysData_g0_t0.imec0.ap.cbin"
     binary = Path(sr.file_bin)
-    folder = Path(save_folder)
+    folder = Path(save_folder)   
     processed_path = folder / 'binary.json'
     standardized_file = folder / f"{binary.stem}.normalized.bin"
     metadata_file = standardized_file.parent.joinpath(
@@ -519,8 +520,28 @@ def download_IBL(
             rec = si.read_binary_folder(save_folder)
             return rec, metadata_file
             print("done preprocessing")
+            
+            
+    #change file names to work with ibl and spike interface
+    files = os.listdir(sr.target_dir)
+    print('Old dir: ', os.listdir(sr.target_dir))
+    #if no files are in the ap.cbin format, run the replacement
+    if not any([file.endswith('ap.cbin') for file in files]):
+        print('converting files to drop "stream" from path')
+        for file in files:
+            #remove .stream from file name
+            new_name = file.replace('.stream', '') 
+            os.rename((str(sr.target_dir) + '/' + file), (str(sr.target_dir) + '/' + new_name)) 
+            
+            #create a new, empty, file with the old name. This is so the ibl loader wont rerun when it sees .stream files are missing
+            with open(os.path.join(sr.target_dir, file), 'w') as fp: 
+                pass
+        print('New dir: ', os.listdir(sr.target_dir))
+        
     
     if do_spikeinterface_destripe:
+        #print('tdir: ', sr.target_dir)
+        #print('pth: ', list(sr.target_dir.glob(f"*.{'ap'}.*cbin")))
         rec = si.read_cbin_ibl(sr.target_dir)
         # preprocessing (mimic IBL's pipeline with spikeinterface)
         print("preprocessing (mimic IBL's pipeline with spikeinterface)")
